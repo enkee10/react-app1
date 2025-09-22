e8c09cc2189baa19b17a89557c6448bc6d556883
enkee10 <enkee03@cetprosmpe.edu.pe>
2025-09-22 00:25:42 -0500

    fix5

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwind from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-app1/', // <-- muy importante para rutas y assets
- // plugins: [react(), tailwind()],
+  //base: '/react-app1/', // <-- muy importante para rutas y assets
+ plugins: [react(), tailwind()],
 })
