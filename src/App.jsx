df3aea33ce9d86be9bfe911f7b9b334b039acf9d
enkee10 <enkee03@cetprosmpe.edu.pe>
2025-10-02 13:11:47 -0500

    deploy corregido3

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Contacto from "./pages/Contacto";
 import Error404 from "./pages/Error404";
 import {Producto} from "./components/Producto"
 import { Contador } from "./components/contador";
-import { Formulario } from "./components/formulario";
+import { Formulario } from "./components/Formulario";
 import { CasoState } from "./components/CasoState";
 import { Pokemon } from "./components/pokemon";
 import { Usuarios } from "./components/usuarios";
